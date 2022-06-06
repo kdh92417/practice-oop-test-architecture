96b8e78499c972ebb668571317efd20d9483c64b
kdh92417 <kdh92417@gmail.com>
2022-06-06 16:40:40 +0900

    [Test] Write Mocking

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,22 @@
 import pytest
 from oop_code import GrabStore, Product, User
+from real_adam_store import AdamRealStore
 
+API_URL = "https://fakestoreapi.com/products/"
+
+@pytest.fixture(scope="function")
+def mock_products():
+    return {
+        1: {'title': '키보드', 'price': 30000},  # Cheap
+        2: {'title': '모니터', 'price': 5000000},  # Expensive
+    }
 
 # scope : 한번 만 값 반환 or 반복적으로 값 반환
+
+
 @pytest.fixture(scope="function")
 def adam_store():
-    return GrabStore(
-        products={
-            1: Product(name='키보드', price=30000),  # Cheap
-            2: Product(name='모니터', price=5000000),  # Expensive
-        }
-    )
+    return AdamRealStore()
 
 
 @pytest.fixture(scope="function")
--- tests/test_grabstore.py
+++ tests/test_grabstore.py
@@ -1,21 +1,34 @@
 import pytest
-from oop_code import GrabStore, Product
+from unittest import mock
 
+from oop_code import (
+    GrabStore,
+    Product
+)
+from tests.conftest import API_URL
 
 """
 Unit Test
 """
 
 
-def test_show_product(adam_store):
+def test_show_product(requests_mock, adam_store, mock_products):
     # given
     product_id = 1
+    mock_product = mock_products[product_id]
+    requests_mock.get(f"{API_URL}{product_id}", json=mock_product)
+
+    # with mock.patch('requests.get') as mock_api:
+    #     res = mock_api.return_value
+    #     res.status_code = 200
+    #     res.json.return_value = mock_product
 
     # when
-    straw_berry = adam_store.show_product(product_id=product_id)
+    product = adam_store.show_product(product_id=product_id)
 
     # then
-    assert straw_berry == Product(name='키보드', price=30000)
+    assert product == Product(
+        name=mock_product['title'], price=mock_product['price'])
 
 
 def test_take_money(adam_store):
@@ -36,12 +49,18 @@ def test_return_money(adam_store):
     assert adam_store._money == pre_money - price
 
 
-def test_take_out_product(adam_store):
+def test_take_out_product(requests_mock, adam_store, mock_products):
+    # given
     product_id = 1
+    mock_product = mock_products[product_id]
+    requests_mock.delete(url=f"{API_URL}{product_id}", json=mock_product)
+
+    # when
     product = adam_store._take_out_product(product_id=product_id)
 
-    assert product == Product(name='키보드', price=30000)
-    assert not adam_store._products.get(product_id, None)
+    # then
+    assert product == Product(name=mock_product['title'], price=mock_product['price'])
+    # assert not adam_store._products.get(product_id, None)
 
 
 """
@@ -49,20 +68,30 @@ Integration Test
 """
 
 
-def test_sell_product_well(adam_store):
+def test_sell_product_well(requests_mock, adam_store, mock_products):
     product_id = 1
     pre_money = adam_store._money
-    product = adam_store.show_product(product_id=product_id)
+    mock_product = mock_products[product_id]
 
+    # mocking
+    requests_mock.get(f"{API_URL}{product_id}", json=mock_product)
+    requests_mock.delete(f"{API_URL}{product_id}", json=mock_product)
+
+    product = adam_store.show_product(product_id=product_id)
     _product = adam_store.sell_product(
         product_id=product_id, money=product.price
     )
     assert adam_store._money == product.price
-    assert not adam_store.show_product(product_id=product_id)
+    # assert not adam_store.show_product(product_id=product_id)
 
 
-def test_sell_product_not_found(adam_store):
+def test_sell_product_not_found(requests_mock, adam_store, mock_products):
     product_id = 100
+    mock_product = mock_products.get(product_id, None)
+
+    # mocking
+    requests_mock.get(f"{API_URL}{product_id}", json=mock_product)
+    requests_mock.delete(f"{API_URL}{product_id}", json=mock_product)
 
     with pytest.raises(Exception):
         adam_store.sell_product(product_id=product_id, money=0)
